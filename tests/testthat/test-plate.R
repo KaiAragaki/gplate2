78ea055c8e11984d9ecc21959694b85fb27171ff
Kai Aragaki <adamaragaki@gmail.com>
2024-04-05 11:01:03 -0400

    add plate from list of wells test

--- tests/testthat/test-plate.R
+++ tests/testthat/test-plate.R
@@ -3,4 +3,8 @@ test_that("plate creation works", {
     plate(well(1, 1)),
     new_plate(wells = list(well(1, 1)))
   )
+  expect_equal(
+    plate(list(well(1, 1), well(1, 2))),
+    new_plate(wells = list(well(1, 1), well(1, 2)))
+  )
 })
