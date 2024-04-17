0dad0e9cca6cb1c110b1ffc7e65e5d7a4b58c24f
Kai Aragaki <adamaragaki@gmail.com>
2024-04-17 13:52:11 -0400

    add plate method for data.frame class

--- R/plate.R
+++ R/plate.R
@@ -23,3 +23,40 @@ plate.well <- function(x, ...) {
 plate.list <- function(x, ...) {
   new_plate(wells = x)
 }
+
+#' @rdname plate
+#' @export
+plate.data.frame <- function(x, name, ...) {
+  wells <- data_frame_to_wells(x, name)
+  new_plate(wells = wells)
+}
+
+
+vec_to_wells <- function(vector, name, x = 1) {
+  stopifnot(length(name) == 1) # required for setting name in second lapply
+  ## Could get around this with mapply I think
+  content <- vector |>
+    lapply(list) |>
+    lapply(\(x) {
+      names(x) <- name
+      x
+    })
+  mapply(
+    well,
+    x = x, y = rev(seq_len(length(vector))),
+    content = content,
+    SIMPLIFY = FALSE
+  )
+}
+
+data_frame_to_wells <- function(df, name) {
+  out <- sapply(
+    seq_len(ncol(df)),
+    \(i) vec_to_wells(df[, i], name, x = i),
+    simplify = FALSE
+  )
+  names(out) <- NULL
+  do.call(c, out)
+}
+
+# Cbind, rbind wells?
--- tests/testthat/test-plate.R
+++ tests/testthat/test-plate.R
@@ -8,3 +8,35 @@ test_that("plate creation works", {
     new_plate(wells = list(well(1, 1), well(1, 2)))
   )
 })
+
+test_that("can create plate from data.frame", {
+  expect_equal(
+    # 1 3
+    # 2 4
+    plate(data.frame(a = 1:2, b = 3:4), "v"),
+    new_plate(
+      wells = list(
+        well(1, 2, list(v = 1)),
+        well(1, 1, list(v = 2)),
+        well(2, 2, list(v = 3)),
+        well(2, 1, list(v = 4))
+      )
+    )
+  )
+})
+
+test_that("can transpose plate", {
+  expect_equal(
+    # 4 3
+    # 2 1
+    t(plate(data.frame(a = 1:2, b = 3:4), "v")),
+    new_plate(
+      wells = list(
+        well(2, 1, list(v = 1)),
+        well(1, 1, list(v = 2)),
+        well(2, 2, list(v = 3)),
+        well(1, 2, list(v = 4))
+      )
+    )
+  )
+})
